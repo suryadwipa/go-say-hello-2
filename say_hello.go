f6b07aeac277fa4443b53c6856baed8ddf81aab4
surya_dwipa <dwipasurya5@gmail.com>
2023-05-09 10:30:16 +0800

    update new function

--- say_hello.go
+++
@@ -1,6 +0,0 @@
-package sayhello
-
-// SayHello is a nice function
-func SayHello() string {
-	return "hello world, this is updated brooo"
-}
---
+++ sayhello.go
@@ -0,0 +1,6 @@
+package sayhello
+
+// SayHello is a nice function
+func SayHello(name string) string {
+	return "hello world, this is updated brooo " + name
+}
